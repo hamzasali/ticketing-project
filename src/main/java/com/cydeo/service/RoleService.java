fafbd569ffec1dc3dd1eca55d3a28671ffef7631
Hamza Ali <hamzamughal2145@gmail.com>
2022-09-29 15:31:12 -0400

    Ticketing Project Mvc

--- src/main/java/com/cydeo/service/RoleService.java
+++ src/main/java/com/cydeo/service/RoleService.java
@@ -1,14 +1,10 @@
 package com.cydeo.service;
 
 import com.cydeo.dto.RoleDTO;
+import com.cydeo.dto.UserDTO;
 
 import java.util.List;
 
-public interface RoleService {
-
-    RoleDTO save(RoleDTO role);
-    RoleDTO findById(Long id);
-    List<RoleDTO> findAll();
-    void deleteById(Long id);
+public interface RoleService  extends CrudService<UserDTO, Long>{
 
 }
