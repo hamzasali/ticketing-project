385bfd8e885d680a87b0b6e81b6f4be6b425c6d1
Hamza Ali <hamzamughal2145@gmail.com>
2022-09-30 16:15:31 -0400

    Ticketing Project Mvc

--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -6,6 +6,8 @@ import com.cydeo.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -21,10 +23,22 @@ public class UserController {
     }
 
     @GetMapping("/create")
-    public String createUser(Model model){
+    public String createUser(Model model) {
         model.addAttribute("user", new UserDTO());
         model.addAttribute("roles", roleService.findAll());
         model.addAttribute("users", userService.findAll());
+        return "user/create";
+    }
+
+    @PostMapping("/create")
+    public String insertUser(Model model, @ModelAttribute("user") UserDTO user) {
+
+        model.addAttribute("user", new UserDTO());
+        model.addAttribute("roles", roleService.findAll());
+        userService.save(user);
+        model.addAttribute("users", userService.findAll());
+
+
         return "user/create";
     }
 
