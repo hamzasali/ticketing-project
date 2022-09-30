c026aad0a0e2b89e57fe2c22cfb80f68ce6bf550
Hamza Ali <hamzamughal2145@gmail.com>
2022-09-30 12:30:59 -0400

    Ticketing Project Mvc

---
+++ src/main/java/com/cydeo/bootstrap/DataGenerator.java
@@ -0,0 +1,28 @@
+package com.cydeo.bootstrap;
+
+import com.cydeo.dto.RoleDTO;
+import com.cydeo.service.RoleService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+@Component
+public class DataGenerator implements CommandLineRunner {
+    private final RoleService roleService;
+
+    public DataGenerator(RoleService roleService) {
+        this.roleService = roleService;
+    }
+
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        RoleDTO adminRole = new RoleDTO(1L, "Admin");
+        RoleDTO managerRole = new RoleDTO(2L, "Manager");
+        RoleDTO employeeRole = new RoleDTO(3L, "Employee");
+
+        roleService.save(adminRole);
+
+    }
+}
