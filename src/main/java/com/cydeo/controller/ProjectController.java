7d5b349eeb435018e67a55c578a69a9039d6f3aa
Hamza Ali <hamzamughal2145@gmail.com>
2022-10-02 14:56:28 -0400

    Ticketing Project Mvc

--- src/main/java/com/cydeo/controller/ProjectController.java
+++ src/main/java/com/cydeo/controller/ProjectController.java
@@ -1,6 +1,9 @@
 package com.cydeo.controller;
 
+import com.cydeo.dto.ProjectDTO;
+import com.cydeo.service.ProjectService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,9 +11,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/project")
 public class ProjectController {
 
-    @GetMapping("create")
-    public String createProject(){
+    private final ProjectService projectService;
 
+    public ProjectController(ProjectService projectService) {
+        this.projectService = projectService;
+    }
+
+
+    @GetMapping("create")
+    public String createProject(Model model) {
+
+        model.addAttribute("project", new ProjectDTO());
+        model.addAttribute("managers");
         return "/project/create";
     }
 }
